cf2e6c5a503ba07a1fdcaf8a64c492f754165d3c
Jyldyz Sultanbekova <sultanbekova.jyldyz@gmail.com>
2021-05-25 17:53:07 +0600

    added a menu to the toolbar with 4 items

--- app/src/main/java/kg/tutorialapp/homework_37/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/homework_37/MainActivity.kt
@@ -3,16 +3,47 @@ package kg.tutorialapp.homework_37
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity(), FragmentToast {
+class MainActivity : AppCompatActivity(), FragmentToast, MenuMainListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(findViewById(R.id.myToolbar))
+
+
         setupActivity2()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.action_call -> {
+                showItem1()
+            }
+            R.id.action_camera -> {
+                showItem2()
+            }
+            R.id.action_settings -> {
+                showItem3()
+            }
+            R.id.action_help -> {
+                showItem4()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupActivity2() {
@@ -37,6 +68,21 @@ class MainActivity : AppCompatActivity(), FragmentToast {
         Toast.makeText(this, "Enter text please!", Toast.LENGTH_LONG).show()
     }
 
+    override fun showItem1() {
+        Toast.makeText(this, "Call", Toast.LENGTH_LONG).show()
+    }
+
+    override fun showItem2() {
+        Toast.makeText(this, "Camera", Toast.LENGTH_LONG).show()
+    }
+
+    override fun showItem3() {
+        Toast.makeText(this, "Settings", Toast.LENGTH_LONG).show()
+    }
+
+    override fun showItem4() {
+        Toast.makeText(this, "Help", Toast.LENGTH_LONG).show()
+    }
 }
 
 
---
+++ app/src/main/java/kg/tutorialapp/homework_37/MenuMainListener.kt
@@ -0,0 +1,8 @@
+package kg.tutorialapp.homework_37
+
+interface MenuMainListener {
+    fun showItem1()
+    fun showItem2()
+    fun showItem3()
+    fun showItem4()
+}
